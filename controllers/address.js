9aedec94f7f263fee9d913df279971a2b086a0c0
Malik-Ehtesham <shamimalick321@gmail.com>
2024-06-09 06:32:53 +0500

    Fighting with errors

--- controllers/address.js
+++ controllers/address.js
@@ -45,6 +45,26 @@ exports.addAddress = catchAsync(async (req, res, next) => {
   res.status(201).json(createdAddress);
 });
 
+// exports.updateAddress = catchAsync(async (req, res, next) => {
+//   const { id } = req.params;
+//   const address = await Address.findById(id);
+
+//   if (!address) {
+//     return res.status(404).json({ error: "Address not found" });
+//   }
+
+//   address.firstName = req.body.firstName || address.firstName;
+//   address.lastName = req.body.lastName || address.lastName;
+//   address.address = req.body.address || address.address;
+//   address.city = req.body.city || address.city;
+//   address.phoneNumber = req.body.phoneNumber || address.phoneNumber;
+//   address.zipcode = req.body.zipcode || address.zipcode;
+//   address.country = req.body.country || address.country;
+
+//   const updatedAddress = await address.save();
+//   res.json(updatedAddress);
+// });
+
 exports.updateAddress = catchAsync(async (req, res, next) => {
   const { id } = req.params;
   const address = await Address.findById(id);
@@ -58,9 +78,18 @@ exports.updateAddress = catchAsync(async (req, res, next) => {
   address.address = req.body.address || address.address;
   address.city = req.body.city || address.city;
   address.phoneNumber = req.body.phoneNumber || address.phoneNumber;
-  address.state = req.body.state || address.state;
   address.zipcode = req.body.zipcode || address.zipcode;
   address.country = req.body.country || address.country;
+
+  if (req.body.isPrimary) {
+    // If the updated address is to be primary, unset previous primary address
+    await Address.updateMany(
+      { user: address.user, isPrimary: true },
+      { isPrimary: false }
+    );
+    address.isPrimary = true;
+    console.log("MAIN CHAL GAYA");
+  }
 
   const updatedAddress = await address.save();
   res.json(updatedAddress);
