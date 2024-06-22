9ab834a879583ae69822b60ada05c63df44ba83d
Malik-Ehtesham <shamimalick321@gmail.com>
2024-06-23 02:14:25 +0500

    READY FOR DEPLOYMENT

--- controllers/product.js
+++ controllers/product.js
@@ -5,7 +5,7 @@ const cloudinary = require("cloudinary").v2;
 cloudinary.config({
   cloud_name: "dcbjngmhn",
   api_key: "665934251338653",
-  api_secret: process.env.CLOUDINARY_SECRET,
+  api_secret: "oIwQNFFVAD1zJI6OAIskq2ie8uk",
 });
 
 exports.createProduct = catchAsync(async (req, res, next) => {
